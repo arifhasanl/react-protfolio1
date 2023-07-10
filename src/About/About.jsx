d4fb6fadfe51cb2ca1197f07d1ee98c692dad5bf
arifhasan <arifhasan1402@gmail.com>
2023-07-10 23:57:48 +0600

    second commit

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -13,11 +13,11 @@ const About = () => {
                 <Section heading={'about me'} para={'Main informations about me'}></Section>
             }
             <div className="flex gap-14">
-                <div className="w-2/5 relative">
+                <div className=" w-full lg:w-2/5 relative">
                    
                     <img className="rounded-md " src={img} alt="" />
                 </div>
-                <div className="w-3/5">
+                <div className="w-full lg:w-3/5">
                     <h4 className="text-4xl mb-4 font-semibold text-gray-800">I'm Arif hasan,A <span className="text-orange-500">{text}</span><Cursor></Cursor></h4>
                     <p className="mb-3 text-gray-600">Hello! I'm <span className="text-orange-500 text-3xl">Arif Hasan</span>, a skilled and dedicated Web Developer focused on creating exceptional websites tailored to your business needs. With 6 month of prictice experience, I possess the skills and expertise to deliver an outstanding online presence for your business. I am committed to understanding your requirements and collaborating closely throughout the design process.</p>
                     <p className="text-gray-600">I believe in active listening to truly understand my client's requirements and deliver top-notch work. My dedication to building long-term relationships means that your success is my priority. Continuously expanding my knowledge and staying up-to-date with the latest technologies, I ensure that your website is built with cutting-edge solutions. Let's work together to bring your vision to life and achieve remarkable results for your business.</p>
--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -9,13 +9,13 @@ const Slider = () => {
       })
     return (
         <div style={{ backgroundImage: `url(${img1})` }} className="">
-            <div className='grid px-5 grid-cols-2 w-full justify-center items-center h-96'>
+            <div className='grid px-5 grid-cols-1 md:grid-cols-2 w-full justify-center items-center h-96'>
                 <div className='center'>
                     <h1 className='text-white flex mb-5 justify-center w-full font-bold text-6xl'>MD:ARIF <span className='text-orange-700'>HASAN</span></h1>
                     <h3 className='text-white font-bold text-4xl flex justify-center w-full'>Hi'I am a <span className='text-orange-500'>{text}</span><Cursor></Cursor></h3>
                 </div>
-                <div className='flex justify-center'>
-                    <img className='h-96 py-4 w-96' src={img2} alt="" />
+                <div className='flex md:block hidden items-end w-full justify-end'>
+                    <img className='h-96 flex justify-end ml-60 py-4 w-96' src={img2} alt="" />
                 </div>
             </div>
         </div>
